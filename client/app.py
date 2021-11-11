8c3739771fbd1a88a4698d9e16e6173fcc92cfda
Umayah Tahsin <umayah.tahsin@hotmail.co.uk>
2021-11-11 17:17:31 +0000

    updated client to use api key secretly

--- client/app.py
+++ client/app.py
@@ -1,7 +1,12 @@
 import geocoder
 import requests
-import json
+from dotenv import load_dotenv
+import os
 from flask import Flask, render_template, request, jsonify
+
+# load api key configuration
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
 
 app = Flask(__name__)
 
@@ -11,20 +16,23 @@ def index():
         lat = request.args.get('lat')
         lon = request.args.get('lon')
         # call weather api
-        weather_response = requests.get(f"https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude=minutely,hourly,daily,alerts&appid=b309d047959f93e82e4b58f1b67827c4")
+        weather_response = requests.get(f"https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude=minutely,hourly,daily,alerts&appid={API_KEY}")
         
         # call pokemon type api
-        weather_types_response = ["Grass"]
+        weather_types_response = ["Grass", "Poison"]
         
         pokemon_type_param = ""
         for pokemon_type in weather_types_response:
             pokemon_type_param += f"type={pokemon_type}&"
         
+        pokemon_type_param = pokemon_type_param[:-1]
+
         # call pokemon likelihood api
-        likelihood_response = requests.get(f"http://127.0.0.1:5001/pokemon?{pokemon_type_param[:-1]}")
+        likelihood_response = requests.get(f"http://127.0.0.1:5001/pokemon?{pokemon_type_param}")
         
-        # render the pokemons.html page with the above response
         return likelihood_response.content
+        # next: render the pokemons.html page with the above response
+
     else:
         return render_template('index.html')
 
